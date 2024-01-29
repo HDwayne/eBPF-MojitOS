c5e86e5af9eb5281c104d6ec1b0c3ecc0bc5cad9
Chevallier Guilhem <guilhem.chevallier15@orange.fr>
2024-01-29 18:21:00 +0100

    nettoyage

--- packet_compteur.c
+++ packet_compteur.c
@@ -26,9 +26,9 @@ int main(int argc, char const *argv[])
 		{ printf("Error while creating hook \n");return 2;}*/
 
     LIBBPF_OPTS(bpf_tc_opts, opts, .prog_fd = fd);
-	r = bpf_tc_attach(&hook, &opts);
-	if (r < 0)
-		{ printf("Error while attaching program to the hook \n");return 3;}
+    r = bpf_tc_attach(&hook, &opts);
+    if (r < 0)
+	{ printf("Error while attaching program to the hook \n");return 3;}
 
 
     int cur_key=0;
@@ -42,7 +42,7 @@ int main(int argc, char const *argv[])
 
 	opts.prog_fd = opts.prog_id = 0;
 	bpf_tc_detach(&hook, &opts);
-    bpf_tc_hook_destroy(&hook);
+    	bpf_tc_hook_destroy(&hook);
 	packet_compteur_egress_bpf__destroy(skel);
 
     return 0;
