7bbf0c94689e16ce6b9c0d82f1da96098e660cbe
Chevallier Guilhem <guilhem.chevallier15@orange.fr>
2024-02-04 15:19:22 +0100

    réorganisation + programme fonctionnant pour toutes les interfaces simultanément

---
+++ Packet_counter/packet_compteur.c
@@ -0,0 +1,151 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+#include <stdlib.h> 
+#include <sys/resource.h>
+#include <sys/types.h>
+#include <ifaddrs.h>
+#include <net/if.h>
+#include <linux/bpf.h>
+#include "packet_compteur_egress.skel.h"
+
+
+
+static void signaltrap(int sig){
+    printf("\nArrêt du programme\n");
+    exit(0);
+    
+}
+
+int nb_interface (struct ifaddrs *i){
+
+    int nb = 0;
+   
+    for(;i!=NULL;i = i->ifa_next){
+        nb++;
+    }
+    return nb;
+}
+
+int main(int argc, char const *argv[])
+{
+    
+
+    signal(SIGINT,signaltrap);
+    libbpf_set_print(NULL);
+    float freq;
+
+    if (argc != 3){printf("Error : /%s <flow_type> <frequence> ",argv[0]);return 1;}
+
+
+
+    if ((freq=atof(argv[2]))==0){
+        return -1;
+    }
+
+
+    int flow;
+    if(strcmp(argv[1],"ingress")==0){
+        flow = BPF_TC_INGRESS;
+    }
+    else if(strcmp(argv[1],"egress")==0){
+        flow = BPF_TC_EGRESS;
+       
+    }else{
+        printf("le flow doit être 'ingress' ou 'egress' \n" );
+        return -4;
+    }
+
+
+    struct ifaddrs *list_interface;
+    if (getifaddrs(&list_interface) < 0) { printf(" Erreur: impossible de récupérer la liste des interfaces réseau du système\n");return -15;}
+    int nb_itf = nb_interface(list_interface);
+
+
+    struct packet_compteur_egress_bpf *skel;
+    skel = packet_compteur_egress_bpf__open();
+
+
+    if(!skel){
+        printf("Impossible de charger le programme\n");
+        return -3;
+    }
+
+
+    if (bpf_map__set_max_entries(skel->maps.my_octets,nb_itf) <0 ){
+        printf("miammmm\n");
+        return -10;
+    }
+
+    
+
+    if(bpf_map__set_max_entries(skel->maps.timeexec,nb_itf) <0){
+        printf("miammmm\n");
+        return -10;
+    }
+
+
+    if( packet_compteur_egress_bpf__load(skel) < 0){
+        printf("ta mère \n"); return 2;
+    }
+
+    int fd = bpf_program__fd(skel->progs.tc_test);
+    if (!fd){
+        printf("aïe !!!\n");
+        return -100;
+    }
+
+
+    printf("%d\n",bpf_map__max_entries(skel->maps.my_octets));
+    struct ifaddrs *itf;int r;
+
+
+    for( itf = list_interface; itf!=NULL; itf = itf->ifa_next){
+        int index = if_nametoindex(itf->ifa_name);
+        printf("%s %d\n",itf->ifa_name,index);
+
+        LIBBPF_OPTS(bpf_tc_hook, hook, .ifindex = index, .attach_point = flow);
+
+        r = bpf_tc_hook_create(&hook);
+
+        LIBBPF_OPTS(bpf_tc_opts, opts, .prog_fd = fd);
+        r = bpf_tc_attach(&hook, &opts);
+        if (r < 0)
+	    { 
+            bpf_tc_hook_destroy(&hook);
+            freeifaddrs(list_interface);
+	        packet_compteur_egress_bpf__destroy(skel);
+            printf("Error while attaching program to the hook \n");return 3;
+        }
+    }
+
+
+    int cur_key;
+    long long value;
+    long long time;
+
+
+    while(true){
+        
+        cur_key = 0;
+        for( itf = list_interface; itf!=NULL; itf = itf->ifa_next){
+        
+
+            if( bpf_map__lookup_elem(skel->maps.my_octets,&cur_key,sizeof(int),&value,sizeof(long long),BPF_ANY) < 0 ){ printf("Erreur lors de la lecture de la map\n");return 6;};
+            if( bpf_map__lookup_elem(skel->maps.timeexec,&cur_key,sizeof(int),&time,sizeof(long long),BPF_ANY) < 0 ) { printf("Erreur lors de la lecture de la map\n");return 6;};
+            printf("interface : %s , nombre octets : %lld , time = %lld\n",itf->ifa_name,value,time);
+            cur_key++;
+
+        }
+        printf("\n");
+        sleep(freq);
+    }
+
+cleanup:
+	
+    packet_compteur_egress_bpf__destroy(skel);
+
+    return 0;
+}
--- Packet_counter/packet_compteur_egress.bpf.c
+++ Packet_counter/packet_compteur_egress.bpf.c
@@ -6,21 +6,31 @@
 
 
 struct {
-        __uint(type, BPF_MAP_TYPE_ARRAY);
-        __type(key, int);
-        __type(value, long long) ;
-        __uint(max_entries, 2);
-} my_config SEC(".maps");
+	__uint(type,BPF_MAP_TYPE_ARRAY);
+	__type(key,int);
+	__type(value,long long);
+	__uint(max_entries,1);
+} my_octets SEC(".maps");
+
+
+struct {
+	__uint(type,BPF_MAP_TYPE_ARRAY);
+	__type(key,int);
+	__type(value,long long);
+	__uint(max_entries,1);
+} timeexec SEC(".maps");
+
+
 
 
 SEC("tc")
 int tc_test(struct __sk_buff *skb) {
-	int key = 0;
 	void *data = (void *)(long)skb->data;
 	void *data_end = (void *)(long)skb->data_end;
 	long long time = bpf_ktime_get_ns();
 	long long nb_bits = data_end - data;
-	long long *rec =bpf_map_lookup_elem(&my_config,&key);
+	int key = skb->ifindex-1;
+	long long *rec =bpf_map_lookup_elem(&my_octets,&key);
 	
 
     if(!rec){
@@ -29,21 +39,17 @@ int tc_test(struct __sk_buff *skb) {
     }
     
     __sync_fetch_and_add(rec,nb_bits);
-	/*int r = bpf_map_update_elem(&my_config,&key,rec,BPF_ANY);
-	if (r<0){
-		bpf_printk("mdr 3\n");
-		return 2;
-	}*/
 
-	key++;
 
-	
 	time = bpf_ktime_get_ns() - time;
-	int r = bpf_map_update_elem(&my_config,&key,&time,BPF_ANY);
+	int r = bpf_map_update_elem(&timeexec,&key,&time,BPF_ANY);
 	if (r<0){
 		bpf_printk("Erreur : update de la map impossible \n");
 		return 3;
 	}
+
+
+	bpf_printk("itf : %d value : %d time: %d\n",skb->ifindex,*rec,time);
 	
     	
     return 0;
